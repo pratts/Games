1a99bb9301cff8b165caddf5cc8af297f385e9c2
Prateek Sharma <prateek.usict@gmail.com>
2016-01-01 15:07:49 +0530

    canvas created and point movement started

---
+++ main.js
@@ -0,0 +1,39 @@
+var snake = {
+    initialXPosition : 0,
+    initialYPosition : 0,
+    headPosition : 0,
+    tailPosition : 0,
+    lengthBody : 2,
+    direction : "right"
+};
+
+var canvasUtil = {
+    canvasObj : document.getElementById("canvasElem"),
+    canvasContext : null,
+    initializeData : function () {
+        "use strict";
+        if (canvasUtil.canvasObj.getContext) {
+            canvasUtil.canvasContext = canvasUtil.canvasObj.getContext('2d');
+            canvasUtil.startSnake();
+        }
+    },
+    drawRect : function () {
+        "use strict";
+        canvasUtil.canvasContext.clearRect(0, 0, canvasUtil.canvasObj.width, canvasUtil.canvasObj.height);
+        canvasUtil.canvasContext.fillStyle = "rgb(255, 255, 255)";
+        canvasUtil.canvasContext.fillRect(snake.headPosition, snake.initialYPosition, snake.lengthBody, 2);
+        snake.headPosition += 1;
+    },
+    
+    startSnake : function () {
+        "use strict";
+        setInterval(canvasUtil.drawRect, 1000);
+    },
+    
+    stopSnake : function () {
+        "use strict";
+        clearInterval();
+    }
+};
+
+canvasUtil.initializeData();
