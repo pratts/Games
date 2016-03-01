b2a109b47ef5846ad97299a87032ea9a83ca72cc
Prateek Sharma <prateek.usict@gmail.com>
2016-03-01 07:25:44 +0530

    snake controller modified with more functions

--- js/api/snakeController.js
+++ js/api/snakeController.js
@@ -1,19 +1,29 @@
 var controller = {
-    createSnakeAndFood : function () {
+	randomIntFromInterval : function (min, max) {
+		"use strict";
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	},
+    createSnakeAndFood : function (snakeProp) {
         "use strict";
-        snakeProp.snakeBody = [];
-        var i = 0, sk = null;
+		var snake = {}, i = 0, sk = null;
+        snake.snakeBody = [];
         while (i < 30) {
             sk = new SnakeObj(i, 0, "moveRight");
-            snakeProp.snakeBody.push(sk);
+            snake.snakeBody.push(sk);
             i = i + 1;
         }
-        snakeProp.snakeHead = new SnakeObj(i - 1, 0, "moveRight");
-           
-        food.x = randomIntFromInterval(1, width - 1);
-        food.y = randomIntFromInterval(1, height - 1);
+        snake.snakeHead = new SnakeObj(i - 1, 0, "moveRight");
+		return snake;
     },
-    clearCanvas : function () {
+	createFood : function (width, height) {
+		"use strict";
+		var foodPoint = {};
+		foodPoint.x = controller.randomIntFromInterval(1, width - 1);
+		foodPoint.y = controller.randomIntFromInterval(1, height - 1);
+		return foodPoint;
+	},
+    clearCanvas: function (canvasContext, width, height) {
+		"use strict";
         canvasContext.clearRect(0, 0, width, height);
-    }   
+    }
 };
